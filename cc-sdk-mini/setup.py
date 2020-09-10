7f1ce5e5169213af4e070b93ae3f406e9519cdbd
Brendan Johnson <brendan_johnson@trendmicro.com>
2020-09-10 09:24:05 -0500

    Fix importing in CC SDK

--- cc-sdk-mini/setup.py
+++ cc-sdk-mini/setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup_options = dict(
     name="cc-sdk-mini", # Replace with your own username
-    version='0.00.01',
+    version='0.00.03',
     author="Brendan Johnson",
     author_email="brendan_johnson@trendmicro.com",
     description="A lightweight SDK for Trend Micro Cloud Conformity",
